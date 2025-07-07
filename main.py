7671ee2c0c38dac641c2bf77998c5d8e4d415411
SmartBypass <arifulbangladesh.a@gmail.com>
2025-07-08 01:21:33 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import telebot
 from web import start
 import threading
 
-BOT_TOKEN = '7548863319:AAH07TxgnYBygfAv5zo0lSks1YpK24bs1k0'
-bot = telebot.TeleBot(BOT_TOKEN)
-
+import os
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 @bot.message_handler(commands=['start'])
 def start_cmd(message):
     bot.reply_to(message, "👋 Bot is working!")
